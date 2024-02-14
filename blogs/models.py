36c7a9069649dae15556cb7d7c433f59471a263b
osm3rr <parabavireo@gmail.com>
2024-02-14 10:00:11 -0400

    datos en la tabla Post

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    boby = models.TextField()
+    author = models.ForeignKey(
+        "auth.User", 
+        on_delete=models.CASCADE,
+        )
+    def __str__(self) -> str:
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
